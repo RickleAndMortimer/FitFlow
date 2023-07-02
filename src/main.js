512c3facf825048b0514e4b3d9d08587118bf53a
nxya <nathacutlan@gmail.com>
2023-07-01 20:41:02 -0400

    a

--- src/main.js
+++ src/main.js
@@ -1,39 +1,10 @@
-import { useState, useEffect } from 'react'
-import Input from './components/Input'; 
-import './App.css';
-
-
-const App = () => {
-  const timeItems = [
-    { value: 1, label: '1 Minute' },
-    { value: 10, label: 'Ten Minutes' },
-    { value: 20, label: 'Twenty Minutes' },
-    { value: 30, label: 'Thirty Minutes' },
-  ];
-  const exerciseItems = [
-    { value: "bicep curl", label: 'Bicep Curl' },
-  ];
-
-  const setsItems = [
-    { value: 2, label: '2' },
-  ];
-
-  const repItems = [
-    { value: 1, label: '1' },
-    { value: 5, label: '5' },
-    { value: 6, label: '6' },
-    { value: 7, label: '7' },
-  ];
-
-let start = false;
-
-  useEffect(() => {
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
 let stage = "down";
 let counter = 0;
+let start = false;
 let sets = 0;
 
 function onResults(results) {
@@ -55,14 +26,14 @@ function onResults(results) {
   drawLandmarks(canvasCtx, results.poseLandmarks,
                 {color: '#FF0000', lineWidth: 2});
 
-  if (results.poseLandmarks && start) {
+  if (results.poseLandmarks) {
 	  const shoulder = results.poseLandmarks[11];
 	  const elbow = results.poseLandmarks[13];
 	  const wrist = results.poseLandmarks[15];
-	  if (shoulder.visibility > 0.9 && elbow.visibility > 0.9 && wrist.visibility > 0.9) {
-		  const v1 = {x: shoulder.x - elbow.x, y: shoulder.y - elbow.y, z: shoulder.z - elbow.z};
-		  const v2 = {x: wrist.x - elbow.x, y: wrist.y - elbow.y, z: wrist.z - elbow.z};
+	  const v1 = {x: shoulder.x - elbow.x, y: shoulder.y - elbow.y, z: shoulder.z - elbow.z};
+	  const v2 = {x: wrist.x - elbow.x, y: wrist.y - elbow.y, z: wrist.z - elbow.z};
 
+	  if (shoulder.visibility > 0.9 && elbow.visibility > 0.9 && wrist.visibility > 0.9) {
 		  const angle = Math.atan2(v1.y, v1.x) - Math.atan2(v2.y, v2.x);
 		  const degrees = Math.abs(angle * 180 / Math.PI);
 		  console.log(stage);
@@ -109,45 +80,5 @@ const camera = new Camera(videoElement, {
 });
 
 const button = document.getElementById("startBtn");
-button.addEventListener('click', (e) => {
-  start = true;
-})
-
 
 camera.start();
-  }, []);
-
-  return (
-    <div className="App">
-      <div>
-        <h1 className="header">Fit-Flow</h1>
-        <div className="inputContainer">
-          <Input id="time" label="⏳ Time" menuItems={timeItems} />
-          <Input id="exercise" label="🤸 Exercise" menuItems={exerciseItems} />
-          <Input id="sets" label="🎯 Number of Sets" menuItems={setsItems} />
-          <Input id="reps" label="🏋️ Number of Reps" menuItems={repItems} />
-	  <p>Reps <span id="count">0</span></p>
-          <button id="startBtn">😄 Start Exercise</button>
-        </div>	
-	<video className="input_video"></video>
-
-	<div className="poseApp">
-	    <canvas className="output_canvas" width="1280px" height="720px"></canvas>
-	    <div className="progressBar">
-	  	<h1>Progress</h1>
-	  	<div>
-	  		<h2>😊 Great job</h2>
-		</div>
-	  	<div>
-			<h2><span id="sets_done">0</span> sets done!</h2>
-		</div>
-	    </div>
-	</div>
-	<div className="landmark-grid-container"></div>
-      </div>
-      <script src="%PUBLIC_URL%/main.js"></script>
-    </div>
-  )
-}
-
-export default App
